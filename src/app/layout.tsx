a4dc97ec3b4fe392423fbb69ee1c616c154bb6cc
MarouenDarragi <marouendarragi89@gmail.com>
2025-02-25 09:57:14 +0100

    ajout page contact & menu & footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/style.scss";
 
+import MenuOne from "@/components/Header/Menu/MenuOne";
+
+import Footer from "@/components/Footer/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MenuOne /> {/* Menu intégré ici */}
+        <main>{children}</main>
+        <Footer /> {/* Footer intégré ici */}
+      </body>
     </html>
   );
 }
